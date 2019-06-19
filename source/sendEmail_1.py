0a470b6425e204436e14d7d5617ac9f1864e009c
xwp <xwp_fullstack@163.com>
2019-06-19 17:55:29 +0800

    udpate

--- source/sendEmail_1.py
+++ source/sendEmail_1.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-
+import sys
 import smtplib
 from email.mime.text import MIMEText
 
+print(sys.argv)
+print(sys.argv[1])
 mailserver = "smtp.126.com"  #邮箱服务器地址
 username_send = 'create6@126.com'  #邮箱用户名
 password = 'bolsu3306'   #邮箱密码：需要使用授权码
 username_recv = '1362254116@qq.com'  #收件人，多个收件人用逗号隔开
-mail = MIMEText('Hello,today is 6-19')   #内容
+mail = MIMEText(sys.argv[1])   #内容
 mail['Subject'] = 'lsu_IT'
 mail['From'] = username_send  #发件人
 mail['To'] = username_recv  #收件人；[]里的三个是固定写法，别问为什么，我只是代码的搬运工

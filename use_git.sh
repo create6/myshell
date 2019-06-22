a5383e34343ea4a8bdc9371a252cd32d90cfec51
bobo <139176@139.com>
2019-06-21 21:36:06 -0700

    udpate

--- use_git.sh
+++ use_git.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-git status
 git add .
 git commit -m 'udpate'
 git push
 
+echo -e "\033[33m push success! \033[0m"
+echo -e "\033[47;30m  begin sendEmail \033[0m"
 
 
-#git rm --cached haha.sh
-#git commit -m 'delete remote somefile'
-#git push
+# 调用python send Email 同时向sys传入相关参数
+~/Desktop/myshell/source/sendEmail_1.py success push by $(pwd)
+
 

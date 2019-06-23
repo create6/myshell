fa6982b6c368e85beedb50067125b703888935e7
bobo <139176@139.com>
2019-06-23 16:55:46 +0800

    udpate

--- use_git.sh
+++ use_git.sh
@@ -11,7 +11,6 @@ echo -e "\033[47;30m  begin sendEmail \033[0m"
 # 调用python send Email 同时向sys传入相关参数
 ~/Desktop/myshell/source/sendEmail_1.py success push by $(pwd)
 
-=======
 echo -e "\033[35m begin sendEmail \033[0m"
 
 

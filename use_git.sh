5da3d0011e437ec0c8b37594865de7d3cf1700fc
bobo <139176@139.com>
2019-06-24 14:35:38 +0800

    udpate

--- use_git.sh
+++ use_git.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-git add . && git commit -m 'udpate' && git push 
-&& echo -e "\033[33m push success! \033[0m" 
-&& echo -e "\033[35m begin sendEmail \033[0m" 
-&& ~/Desktop/myshell/source/sendEmail_1.py success push by $(pwd),In $(date) 
-&& echo -e "\033[35m CODE:200 \033[0m"
-&& echo -e "\033[35m CODE:20000 \033[0m"
+git add . && git commit -m 'udpate' && git push && echo -e "\033[33m push success! \033[0m" && echo -e "\033[35m begin sendEmail \033[0m"  && ~/Desktop/myshell/source/sendEmail_1.py success push by $(pwd),In $(date) && echo -e "\033[35m CODE:200 \033[0m" && echo -e "\033[35m CODE:20000 \033[0m"
 
 
 

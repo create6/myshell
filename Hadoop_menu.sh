a6f01177ebc9d7bd036fe79ed0e2ec169e708a57
jack3306 <1362254116@qq.com>
2019-08-18 19:45:10 +0800

    modify

--- Hadoop_menu.sh
+++ Hadoop_menu.sh
@@ -1,70 +1,63 @@
 #!/usr/bin/bash
 
-
-
 while true
 do
 
+        echo -e "\033[36m ======v1.0===== \033[0m" 
 
-echo -e "\033[36m ======v1.0===== \033[0m" 
+        echo "1:start-all"
+        echo "2:start-dfs"
+        echo "3:start-yarn"
+        echo "4:stop-all"
+        echo "5:stop-dfs"
+        echo "6:show date"
+        echo "119:shutdown"
+        echo "0:edit it"
 
-echo "1:start-all"
-echo "2:start-dfs"
-echo "3:start-yarn"
-echo "4:stop-all"
-echo "5:stop-dfs"
-echo "6:show date"
-echo "119:shutdown"
-echo "0:exit"
+        echo -e "\033[36m ============== \033[0m" 
 
+        cd ~/bigdata/hadoop/sbin/
 
-echo -e "\033[36m ============== \033[0m" 
-
-cd bigdata/hadoop/sbin/
-
-read -p "Please input option:" OPTION
+        read -p "Please input option:" OPTION
 
 
-if [ "$OPTION" = "1" ];then
-    ./start-all.sh
-    echo "start-all"
+        if [ "$OPTION" = "1" ];then
+                    ./start-all.sh
+                            echo "start-all"
 
-elif [ "$OPTION" = "2" ];then
-    ./start-dfs.sh
+                        elif [ "$OPTION" = "2" ];then
+                                    ./start-dfs.sh
 
-elif [ "$OPTION" = "3" ];then
-    ./start-yarn.sh
+                                elif [ "$OPTION" = "3" ];then
+                                            ./start-yarn.sh
 
-elif [ "$OPTION" = "4" ];then
-    ./stop-all.sh
+                                        elif [ "$OPTION" = "4" ];then
+                                                    ./stop-all.sh
 
-elif [ "$OPTION" = "5" ];then
-    ./stop-dfs.sh
+                                                elif [ "$OPTION" = "5" ];then
+                                                            ./stop-dfs.sh
 
-elif [ "$OPTION" = "6" ];then
-    echo $(date)
+                                                        elif [ "$OPTION" = "6" ];then
+                                                                    echo $(date)
 
-elif [ "$OPTION" = "0" ];then
-	cd ~
-    vi Hadoop_menu.sh
-
-
-elif [ "$OPTION" = "119" ];then
-    
-
-#  ./use_git.sh
-    echo "shutdown"
-    shutdown -h now
+                                                                elif [ "$OPTION" = "0" ];then
+                                                                              cd ~
+                                                                                  vi Hadoop_menu.sh
 
 
+                                                                              elif [ "$OPTION" = "119" ];then
+                                                                                  
+                                           
+                                                                                  echo "shutdown"
+                                                                                  shutdown -h now
 
-else
-  
- #  sudo shutdown -h now
-    echo 'xx'
-fi
 
+                                                                              else
+                                                                                
+                                                             
+                                                                                   echo 'exit'
+                                                                               exit
+                                                                               fi
+                                                                               
 
 done
-
-

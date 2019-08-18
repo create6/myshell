7b9845522f7e1790c30c8620afffd1a0301195e7
jack3306 <1362254116@qq.com>
2019-08-18 11:06:00 +0800

    add Hadoop_start

--- use_git.sh
+++ use_git.sh
@@ -4,3 +4,5 @@
 read -p "commit what?" OPTION
 git add . && git commit -m "$OPTION"  && git push || read -p "your new branch:" BRANCH && git push --set-upstream origin $BRANCH  && echo -e "\033[33m push success! \033[0m \n" 
 
+#执行不成功可以加入菜单，把可能情况都加入
+
